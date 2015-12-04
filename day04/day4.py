1b46a3c04370b5671a921dda46433166c2b37760
pylbrecht <pylbrecht@gmail.com>
2015-12-04 23:48:07 +0100

    initial commit

---
+++ day04/day4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Advent of Code - Day 4
+======================
+
+The Ideal Stocking Stuffer
+"""
+
+__author__ = "pylbrecht"
+__contact__ = "pylbrecht@gmail.com"
---
+++ day04/test_day4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = "pylbrecht"
+__contact__ = "pylbrecht@gmail.com"
