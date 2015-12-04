dee0f3dffb7f8a65d51095c05935a01547a63649
pylbrecht <pylbrecht@gmail.com>
2015-12-05 00:09:31 +0100

    solved day4

--- day04/day4.py
+++ day04/day4.py
@@ -10,3 +10,38 @@ The Ideal Stocking Stuffer
 
 __author__ = "pylbrecht"
 __contact__ = "pylbrecht@gmail.com"
+
+
+import hashlib
+
+
+def get_hash(data, algorithm="md5"):
+    """
+    Compute the hash of a byte string and return its value as a hex string.
+    """
+    h = hashlib.new(algorithm)
+    h.update(data)
+    return h.hexdigest()
+
+
+if __name__ == '__main__':
+    secret_key = "bgvyzdsv"
+    number = 0
+
+    data = "{}{}".format(secret_key, number)
+
+    checksum = get_hash(data.encode('utf-8'))
+
+    while not checksum.startswith("00000"):
+        number += 1
+        data = "{}{}".format(secret_key, number)
+        checksum = get_hash(data.encode('utf-8'))
+
+    print("five zeroes: %d" % number)
+
+    while not checksum.startswith("000000"):
+        number += 1
+        data = "{}{}".format(secret_key, number)
+        checksum = get_hash(data.encode('utf-8'))
+
+    print("six zeroes: %d" % number)
--- day04/test_day4.py
+++ day04/test_day4.py
@@ -6,3 +6,13 @@
 
 __author__ = "pylbrecht"
 __contact__ = "pylbrecht@gmail.com"
+
+
+from day4 import *
+
+
+def test_get_md5_hash():
+    """
+    """
+    data = b"test"
+    assert get_hash(data) == "098f6bcd4621d373cade4e832627b4f6"
