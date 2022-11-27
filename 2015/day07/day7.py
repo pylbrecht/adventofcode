46106a377cc1bcc1e2129e5e186f836d92b8fa1c
pylbrecht <palbrecht@mailbox.org>
2022-11-27 21:37:04 +0100

    [2015/day7] Parse expressions

--- 2015/day07/day7.py
+++ 2015/day07/day7.py
@@ -1,3 +1,37 @@
+from dataclasses import dataclass
+
+
 class Program:
     def run(self, code):
         raise NotImplementedError
+
+
+IntegerLiteral = int
+Identifier = str
+Operand = IntegerLiteral | Identifier
+
+
+@dataclass
+class BitwiseOperation:
+    op: str
+    right: Operand
+    left: Operand | None
+
+
+def parse_expression(expr):
+    if expr.isdigit():
+        return int(expr)
+
+    tokens = expr.split(" ")
+
+    try:
+        left, op, right = tokens
+    except ValueError:
+        left, op, right = [None, *tokens]
+
+    if left and left.isdigit():
+        left = int(left)
+    if right.isdigit():
+        right = int(right)
+
+    return BitwiseOperation(left=left, op=op, right=right)
--- 2015/day07/test_day7.py
+++ 2015/day07/test_day7.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day7 import Program
+from day7 import Program, BitwiseOperation, parse_expression
 
 
 @pytest.mark.xfail(reason="unimplemented")
@@ -29,3 +29,16 @@ def test_example():
         "y": 456,
     }
     assert program.wires == expected_wires
+
+
+@pytest.mark.parametrize(
+    "expr,result",
+    [
+        ("123", 123),
+        ("x AND y", BitwiseOperation(op="AND", left="x", right="y")),
+        ("x LSHIFT 2", BitwiseOperation(op="LSHIFT", left="x", right=2)),
+        ("NOT y", BitwiseOperation(op="NOT", left=None, right="y")),
+    ],
+)
+def test_parse_expression(expr, result):
+    assert parse_expression(expr) == result
